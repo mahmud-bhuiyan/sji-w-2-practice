48fceac4a14851da9236056ad71fb3a61748fba0
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-11-04 00:35:15 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1>This is the home page</h1>
-      <Link to="about">About</Link>
-      <Link to="contact">Contact</Link>
     </div>
   );
 };
